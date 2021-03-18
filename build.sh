2ed48ef833438ce5ab089f9ebc12523577332f0c
Najah Muhabbik <abiek2004@gmail.com>
2021-03-18 14:29:04 +0000

    Rewrite scripts
    Signed-off-by: Najah Muhabbik <abiek2004@gmail.com>

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export KERNELNAME=Moonlight
+export KERNELNAME=Nightmare
 
-export LOCALVERSION=Hikari-v1.9
+export LOCALVERSION=
 
-export KBUILD_BUILD_USER=frost
+export KBUILD_BUILD_USER=frost_id
 
-export KBUILD_BUILD_HOST=otaku
+export KBUILD_BUILD_HOST=droneci
 
 export TOOLCHAIN=clang
 
@@ -20,9 +20,9 @@ source helper
 
 gen_toolchain
 
-send_msg "⏳ Start building ${KERNELNAME} ${LOCALVERSION} | DEVICES: whyred - tulip - lavender - wayne - jasmine"
+send_msg "⏳ Start building ${KERNELNAME} ${LOCALVERSION..."
 
-send_pesan "⏳ Start building ${KERNELNAME} ${LOCALVERSION} | DEVICES: whyred - tulip - lavender - wayne - jasmine"
+send_pesan "⏳ Start building ${KERNELNAME} ${LOCALVERSION..."
 
 START=$(date +"%s")
 
@@ -33,9 +33,9 @@ do
 	build ${i} -newcam
 done
 
-send_msg "⏳ Start building Overclock version | DEVICES: whyred - tulip"
+send_msg "⏳ Start building Overclock version..."
 
-send_pesan "⏳ Start building Overclock version | DEVICES: whyred - tulip"
+send_pesan "⏳ Start building Overclock version..."
 
 git apply oc.patch
 
@@ -53,6 +53,6 @@ END=$(date +"%s")
 
 DIFF=$(( END - START ))
 
-send_msg "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s, get nightly builds in @MoonlightCI | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
+send_msg "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
 
 send_pesan "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
