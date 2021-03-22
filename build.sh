89128af674858ece4a126e465e2694262a6448d4
Najah Muhabbik <abiek2004@gmail.com>
2021-03-22 02:07:24 +0000

    Delete em.patch in script
    Signed-off-by: Najah Muhabbik <abiek2004@gmail.com>

--- build.sh
+++ build.sh
@@ -39,8 +39,6 @@ send_pesan "⏳ Start building Overclock version | DEVICES: whyred - tulip"
 
 git apply oc.patch
 
-git apply em.patch
-
 for i in ${DEVICES//,/ }
 do
 	if [ $i == "whyred" ] || [ $i == "tulip" ]
