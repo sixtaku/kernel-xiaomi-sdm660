c0c5fc2ad39993de9584cdbef75effa4e43c214a
Najah Muhabbik <abiek2004@gmail.com>
2021-03-23 11:52:10 +0000

    Nightmare Xtreme v1.0
    Signed-off-by: Najah Muhabbik <abiek2004@gmail.com>

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 export KERNELNAME=Nightmare
 
-export LOCALVERSION=v1.0
+export LOCALVERSION=Xtreme-v1.0
 
-export KBUILD_BUILD_USER=frost_id
+export KBUILD_BUILD_USER=Six
 
 export KBUILD_BUILD_HOST=droneci
 
@@ -22,8 +22,6 @@ gen_toolchain
 
 send_msg "⏳ Start building ${KERNELNAME} ${LOCALVERSION} | DEVICES: whyred - tulip - lavender - wayne - jasmine"
 
-send_pesan "⏳ Start building ${KERNELNAME} ${LOCALVERSION} | DEVICES: whyred - tulip - lavender - wayne - jasmine"
-
 START=$(date +"%s")
 
 for i in ${DEVICES//,/ }
@@ -33,26 +31,8 @@ do
 	build ${i} -newcam
 done
 
-send_msg "⏳ Start building Overclock version | DEVICES: whyred - tulip"
-
-send_pesan "⏳ Start building Overclock version | DEVICES: whyred - tulip"
-
-git apply oc.patch
-
-for i in ${DEVICES//,/ }
-do
-	if [ $i == "whyred" ] || [ $i == "tulip" ]
-	then
-		build ${i} -oldcam -overclock
-
-		build ${i} -newcam -overclock
-	fi
-done
-
 END=$(date +"%s")
 
 DIFF=$(( END - START ))
 
 send_msg "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
-
-send_pesan "✅ Build completed in $((DIFF / 60))m $((DIFF % 60))s | Linux version : $(make kernelversion) | Last commit: $(git log --pretty=format:'%s' -5)"
